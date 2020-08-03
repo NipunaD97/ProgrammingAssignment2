7da91d2b7aee5975f5a0b34811151be2734a3017
NipunaD97 <nipunaperera231@gmail.com>
2020-08-03 17:01:29 +0530

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-nipuna
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) inverse <<- inv
+  getinv <- function() inverse
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv)
 }
 
 
